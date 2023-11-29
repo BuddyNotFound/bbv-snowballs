1a0940aad8f919947e4f3aaa26df067c58ed6397
BuddyBoyVilla <74051918+BuddyNotFound@users.noreply.github.com>
2023-11-29 21:58:46 +0200

    optimizations

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -3,23 +3,13 @@ Main = {
     AnimDict = 'anim@mp_snowball',
     AnimName = 'pickup_snowball'
 }
+RegisterKeyMapping("snowball","Select shooting style","keyboard","G")
 
-CreateThread(function()
-    while true do 
-        Wait(Main.Wait)
-        if not IsPedInAnyVehicle(PlayerPedId(),true) and not IsPedUsingAnyScenario(PlayerId()) and not IsPedInCover(PlayerPedId()) and not IsPedSwimming(PlayerPedId()) and not IsPedFalling(PlayerPedId()) and not IsPedRunning(PlayerPedId()) and not IsPedRagdoll(PlayerPedId()) and not IsPedSprinting(PlayerPedId()) and GetInteriorFromEntity(PlayerPedId()) == 0 and not IsPlayerFreeAiming(PlayerPedId()) and not IsPedSwimmingUnderWater(PlayerPedId()) and not IsPedShooting(PlayerPedId(),0) then
-            Main.Wait = 0
-            if IsControlJustReleased(0,58) then
-                Main:LoadanimDict(Main.AnimDict)
-                TaskPlayAnim(PlayerPedId(),'anim@mp_snowball','pickup_snowball',8.0,-1,-1,0,1,0,0,0) 
-                Main.Wait = 2000
-                GiveWeaponToPed(PlayerPedId(),GetHashKey('weapon_snowball'),1)
-                SetCurrentPedWeapon(PlayerPedId(),GetHashKey('weapon_snowball'),true)
-            end
-        else
-            Main.Wait = 2000
-        end
-    end
+RegisterCommand('snowball',function()
+    Main:LoadanimDict(Main.AnimDict)
+    TaskPlayAnim(PlayerPedId(),'anim@mp_snowball','pickup_snowball',8.0,-1,-1,0,1,0,0,0) 
+    GiveWeaponToPed(PlayerPedId(),GetHashKey('weapon_snowball'),1)
+    SetCurrentPedWeapon(PlayerPedId(),GetHashKey('weapon_snowball'),true)
 end)
 
 function Main:LoadanimDict(animDict) -- Load Anim
